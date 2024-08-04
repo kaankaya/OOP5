cdef3f85be9e3dfa89c66cb566f4b51c905dbd7c
Kaan Kaya <kaankaya.0707@gmail.com>
2024-08-04 16:20:06 +0300

    Add project files.

---
+++ OOP5/BasePerson.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP5
+{
+    //Abstract Olarak BasePerson class ını tanımlıyoruz.New ile türetilemez. Sadece Miras alınabilir
+    // Define the abstract BasePerson class. It cannot be instantiated with 'new'. It can only be inherited.
+    public abstract class BasePerson
+    {
+        //Propert ve field tanımları
+        public string PersonName { get; set; }
+        public string PersonSurname { get; set; }
+        //Methota virtual anahtar kelimesini ekleyerek bu methodun miras alınan yerlerde özelleştirilmesini sağlıyoruz.
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Adınız : {PersonName}\nSoyadınız : {PersonSurname}");
+        }
+    }//End abstract class BasePerson
+    //BasePerson dan türetilmiş Student class 
+    public class Student : BasePerson
+    {
+        public string StudentNumber { get; set; }
+        //Method u override ederek hem ana özelliklerini aldık buna ek olarak da aşağıda bu class a ait öğrenci numarası özelliğini yazdırdık.
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Öğrenci Numarası : {StudentNumber} ");
+        }
+    }//end class Student
+
+    public class Teacher : BasePerson
+    {
+        public int Salary { get; set; }
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Maaş Miktarı : {Salary} ");
+        }
+    }
+}
---
+++ OOP5/Program.cs
@@ -0,0 +1,25 @@
+﻿using OOP5;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Öğrenci Türetimi
+
+        Student student = new Student();
+        student.PersonName = "Patika";
+        student.PersonSurname = "Dev";
+        student.StudentNumber = "42352";
+        student.DisplayInfo();
+
+        Console.WriteLine("**********************");
+        //Öğretmen Türetimi
+
+        Teacher teacher = new Teacher();
+        teacher.PersonName = "Patika";
+        teacher.PersonSurname = "Plus";
+        teacher.Salary = 150000;
+        teacher.DisplayInfo();
+
+    }
+}
